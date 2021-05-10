3537b6c4c9f94907ee583fa8c7e59c087df34a75
davidbragat <davidbragat@gmail.com>
2021-05-10 14:39:10 -0300

    create basic structure and implement new obj

--- javascript/functions.js
+++ javascript/functions.js
@@ -2,5 +2,22 @@ function Clientes(cliente, age, job, address) {
     this.cliente = cliente;
     this.age = age;
     this.job = job;
-    this.adress = address;
+    this.address = address;
 }
+formDom = {
+    name: document.querySelector('#name'),
+    age: document.querySelector('#age'),
+    job: document.querySelector('#job'),
+    address: document.querySelector('#address'),
+    buttom: document.querySelector('#btnAddUser'),
+};
+
+formDom.buttom.addEventListener('click', e => {
+    e.preventDefault();
+    if (formDom.name.value && formDom.age.value && formDom.job.value && formDom.address.value) {
+        let test = new Clientes(formDom.name.value, formDom.age.value, formDom.job.value, formDom.address.value);
+        console.log(test);
+    } else {
+        alert('Fill all fields');
+    }
+});
