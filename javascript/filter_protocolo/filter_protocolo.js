3cbaaf5dad972691d45a1962f47e726313b179e6
davidbragat <davidwbraga@gmail.com>
2022-03-08 02:46:44 -0300

    Added new ITCD Calc
    Added New ITCD Calc

--- javascript/filter_protocolo/filter_protocolo.js
+++ javascript/filter_protocolo/filter_protocolo.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let rgx = function regexProtocol() {
-    let cleanTextRgx = /([A-ZÀ-üa-z\s\.\,\-\;]|\d{2}\/\d{2}\/\d{4})/gm;
+    let cleanTextRgx = /([A-ZÀ-üa-z\s\.\,\-\;\/]|\d{2}\/\d{2}\/\d{4})/gm;
     let splitProtocol = /(\d{13})/gm;
     let trashText = document.querySelector('#trash_text').value;
     let protocolList = document.querySelector('#protocol_list');
